c563ebd964f938a9bf94932c7d92ec6063bda338
sushil23 <sushil2391@gmail.com>
2023-09-20 19:16:37 +0530

    Init commit

---
+++ src/main/java/com/restful/ConsumingRestApis/Quote.java
@@ -0,0 +1,7 @@
+package com.restful.ConsumingRestApis;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+    public record Quote(String type, Value value) {
+}
---
+++ src/test/java/com/restful/ConsumingRestApis/ConsumingRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restful.ConsumingRestApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumingRestApisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
